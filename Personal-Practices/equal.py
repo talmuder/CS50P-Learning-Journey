d8c74a149a35fa62aa4da056019a7c6a35086eec
Hsun Kai Chang <125354864+talmuder@users.noreply.github.com>
2026-02-26 05:50:50 +0000

    code equal.py

---
+++ Personal-Practices/equal.py
@@ -0,0 +1,9 @@
+x=int(input("What is the x ? "))
+y=int(input("What is the y ? "))
+
+if x>y:
+    print("x is greater than y ")
+if x<y:
+    print("x is less than y ")
+if x==y:
+    print("x is equal to y ")
