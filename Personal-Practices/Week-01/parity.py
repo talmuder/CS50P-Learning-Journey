9aa4c29b51e67fd83676db3c61ecf7a4d4ed695c
Hsun Kai Chang <125354864+talmuder@users.noreply.github.com>
2026-03-03 09:57:13 +0000

    code parity.py

--- Personal-Practices/Week-01/parity.py
+++ Personal-Practices/Week-01/parity.py
@@ -1,6 +1,15 @@
-x=int(input("What is x ? "))
-
-if x % 2 == 0:
-    print('x is "EVEN"')
-else:
-    print('x is "ODD"')
+#define main function
+def main():
+    x=int(input("What's x ? "))
+    if is_even(x):
+        print("EVEN")
+    else:
+        print("ODD")
+#define is even funtion
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+#main program
+main()
