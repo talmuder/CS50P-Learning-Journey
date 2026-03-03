b546f19012d9ea7ac27bf6989d6b3ddbfd572b6f
Hsun Kai Chang <125354864+talmuder@users.noreply.github.com>
2026-03-03 10:08:46 +0000

    change bool

--- Personal-Practices/Week-01/parity.py
+++ Personal-Practices/Week-01/parity.py
@@ -7,9 +7,6 @@ def main():
         print("ODD")
 #define is even funtion
 def is_even(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
+        return True if n % 2 == 0 else False
 #main program
 main()
