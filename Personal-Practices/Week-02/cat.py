55e9844b803747297bcc25e848183d81d635933d
Hsun Kai Chang <125354864+talmuder@users.noreply.github.com>
2026-03-10 10:08:50 +0000

    for in n and use while

--- Personal-Practices/Week-02/cat.py
+++ Personal-Practices/Week-02/cat.py
@@ -1,4 +1,15 @@
-i = 0
-while i < 3:
-    print("meow")
-    i += 1
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n ? "))
+        if n > 0 :
+            return n
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+#main program
+main()
