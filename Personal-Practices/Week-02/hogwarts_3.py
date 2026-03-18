e57c465c20244192f69e208f526777398ced0766
Hsun Kai Chang <125354864+talmuder@users.noreply.github.com>
2026-03-18 02:56:12 +0000

    add comma

--- Personal-Practices/Week-02/hogwarts_3.py
+++ Personal-Practices/Week-02/hogwarts_3.py
@@ -1,9 +1,9 @@
 students = [
-    {"name":"Hermione","house":"Gryffindor","patronus":"Otter"}
-    {"name":"Harry","house":"Gryffindor","patronus":"Stag"}
-    {"name":"Ron","house":"Gryffindor","patronus":"Jack Russell terrier"}
-    {"name":"Draco","house":"Slytherin","patronus":None}
+    {"name":"Hermione","house":"Gryffindor","patronus":"Otter"} ,
+    {"name":"Harry","house":"Gryffindor","patronus":"Stag"} ,
+    {"name":"Ron","house":"Gryffindor","patronus":"Jack Russell terrier"} ,
+    {"name":"Draco","house":"Slytherin","patronus":None} ,
 ]
 
 for student in students :
-    print(students["name"])
+    print(student["name"])
