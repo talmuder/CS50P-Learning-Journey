cf5a0633a3eca39c020751a42c5c280f1c1e43fd
Hsun Kai Chang <125354864+talmuder@users.noreply.github.com>
2026-03-18 05:04:08 +0000

    python mario.py

---
+++ Personal-Practices/Week-02/mario.py
@@ -0,0 +1,11 @@
+#define main function
+def main():
+    print_square(3)
+
+#define print square function
+def print_square(size):
+    for i in range(size):
+        print("#"*size)
+
+#main program
+main()
