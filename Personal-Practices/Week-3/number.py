242896cd4bbce863a9cab069739ef6f2bb05abed
Hsun Kai Chang <125354864+talmuder@users.noreply.github.com>
2026-03-25 07:42:38 +0000

    the best number.py

--- Personal-Practices/Week-3/number.py
+++ Personal-Practices/Week-3/number.py
@@ -1,9 +1,16 @@
-while True:
-    try:
-        x = int(input("What's x ? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
+#define main function
+def main():
+    x = get_number("What's x ? ")
+    print(f"X is {x}")
 
-print(f"x is {x}")
+#define get number function
+def get_number(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print('Your enter is not an Integer, Please Enter Again the "X" ')
+
+#main program
+if __name__ == "__main__":
+    main()
