63e8662d9ee030d0f4e646c34d1ea53f9772caee
Hsun Kai Chang <125354864+talmuder@users.noreply.github.com>
2026-03-25 06:59:38 +0000

    again

--- Personal-Practices/Week-3/number.py
+++ Personal-Practices/Week-3/number.py
@@ -1,5 +1,9 @@
-try:
-    x = int(input("What's x ? "))
-    print(f"x is {x}")
-except ValueError:
-    print("x is not an integer")
+while True:
+    try:
+        x = int(input("What's x ? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+
+print(f"x is {x}")
