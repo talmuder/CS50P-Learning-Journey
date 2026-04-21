5b74c68342d259a424ee4c9f6146dc307573e898
Hsun Kai Chang <125354864+talmuder@users.noreply.github.com>
2026-04-21 09:42:55 +0000

    complete namepy

--- Personal-Practices/Week-4/name.py
+++ Personal-Practices/Week-4/name.py
@@ -1,8 +1,7 @@
 import sys
 
 if len(sys.argv) < 2:
-    print("Too few arguments")
-elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("hello, my name is",sys.argv[1])
+    sys.exit("Too few arguments")
+
+for arg in sys.argv[1:]:
+    print("hello, my name is",arg)
